ab254731dd7d5d4ec66d3c678ca7dd4eec78867e
hamza-smh <hamza.smh2000@gmail.com>
2025-08-14 16:55:01 +0500

    pre mui

--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
+const router = express.Router();
+const nodemailer = require('nodemailer');
 
-const contactRoutes = require('./api/contact/route');
+router.post('/', async (req, res) => {
+    const { name, email, message,phone,subject } = req.body;
 
-app.use(cors());
-app.use(express.json());
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hamza.smh2000@gmail.com',
+            pass: 'kdvo jsyc jyay acgw'
+        }
+    });
 
-app.use('/api/contact', contactRoutes);
+    const mailOptions = {
+        from: email,
+        to: 'hamza.smh2000@gmail.com', 
+        subject: 'New message from portfolio',
+        html: `<p><strong>Name:</strong> ${name}</p>
+               <p><strong>Email:</strong> ${email}</p>
+               <p><strong>Phone:</strong> ${phone}</p>
+               <p><strong>Subject:</strong> ${subject}</p>
+               <p><strong>Message:</strong> ${message}</p>`
+    };
 
-const PORT = 5000; 
-app.listen(PORT, () => {
-    console.log(`Backend server running at http://localhost:${PORT}`);
+    try {
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Email failed to send', details: error });
+    }
 });
+
+module.exports = router;
